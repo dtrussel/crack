4d6a939b455066f5d1efe1c2860442172975d5ff
truessd <david.truessel@haag-streit.com>
2020-04-14 07:54:16 +0200

    Solve questions 2.4 and 2.5

--- src/linked_lists.cpp
+++ src/linked_lists.cpp
@@ -53,3 +53,49 @@ TEST(Linked_Lists, Question_2_2) {
   EXPECT_EQ(2, kth_last(in, 3));
   EXPECT_EQ(1, kth_last(in, 4));
 }
+
+/// Question 2.4
+
+std::list<int> partition(const std::list<int>& input, int value){
+  std::list<int> ret{value};
+  for (auto entry : input) {
+    if (entry >= value) { ret.emplace_back(entry); }
+    else { ret.emplace_front(entry); }
+  }
+  return ret;
+}
+
+TEST(Linked_Lists, Question_2_4){
+  EXPECT_EQ(partition({3, 7, 10, 6, 3, 2}, 4), std::list({2, 3, 3, 4, 7, 10, 6}));
+}
+
+/// Question 2.5
+
+int list_to_number(const std::list<int>& in){
+  int ret = 0;
+  int multiplier = 1;
+  for (auto val : in) {
+    ret += val * multiplier;
+    multiplier *= 10;
+  }
+  return ret;
+}
+
+std::list<int> number_to_list(int num){
+  std::list<int> ret;
+  constexpr int divider = 10;
+  while (num) {
+    ret.emplace_back(num % divider);
+    num /= divider;
+  }
+  return ret;
+}
+
+std::list<int> sum(const std::list<int>& alpha, const std::list<int>& beta){
+  const int sum = list_to_number(alpha) + list_to_number(beta);
+  return number_to_list(sum);
+}
+
+TEST(Linked_Lists, Question_2_5){
+  EXPECT_EQ(sum({7, 1, 6}, {5, 9, 2}), std::list({2, 1, 9}));
+}
